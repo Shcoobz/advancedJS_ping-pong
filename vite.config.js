2625dc4ce75dd3189bf811de25f890de874ed329
Shcoobz <a.boeckmann23@gmail.com>
2024-05-16 15:51:50 +0200

    basic project setup

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/react_ping-pong/',
+  build: {
+    outDir: 'build',
+  },
+});
